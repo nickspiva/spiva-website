9da217cd09164832667acbf05ca15ed0af362be2
Nicky <nick.spiva@gmail.com>
2026-05-29 23:54:18 -0400

    updating quite a bit relative to the TAD project - cleaning TAD historic property value data, adding shapefiles and csv data file, sketching historic value chart

---
+++ posts/shiny-practice/model_app/app.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(bslib)
+libary(ggplot2)
+
+ui <- page_sidebar(
+  title = "My Shiny App",
+  sidebar = sidebar(
+    "Shiny is available on CRAN, so you can install it in the usual way from your R console: derp",
+    code("install.packages(\"shiny\")")
+  ),
+  card(
+    card_header("Introducing Shiny"),
+    card_body(
+      "Shiny is a package from Posit that makes it incredibly easy to build
+     interactive web applications with R. For an introduction and live examples, 
+     visit the Shiny homepage (https://shiny.posit.co).",
+      height = "50%"
+    ),
+    card_image(
+      file = "shiny-hex.svg",
+      alt = "Shiny's hex sticker",
+      href = "https://github.com/rstudio/shiny",
+      height = "300px"
+    ),
+    card_footer("Shiny is a product of Posit.")
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
+
+#sidebar with a card, card has a header, dscriptive text, and an image, and a footer.
